7c513532f7da65141e7cc6acb7450c5319f19c87
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2017-11-17 17:34:37 +0700

    Update code

--- Room Display.framer/app.coffee
+++ Room Display.framer/app.coffee
@@ -1,3 +1,11 @@
+# Import file "Bukalapak"
+sketch = Framer.Importer.load("imported/Bukalapak@1x", scale: 1)
+
+# Set Device background
+Screen.backgroundColor = null
+Canvas.image = "images/wood.jpg"
+
+# Firebase Configuration
 {Firebase} = require 'firebase'
 
 bukaruang_db = new Firebase
@@ -28,13 +36,126 @@ amenitiesData = [
 			}
 		}
 ]
-	
-# response = (confirmation) ->
-# 	print confirmation
+
+meetingData = [
+		{
+			meeting_id: 1
+			ruang_id: 1
+			desc: "UI Weekly Meetup"
+			organizer: 1
+			date: "18 November 2017"
+			time_start: "18:30"
+			time_end: "20:30"
+			status: "Not yet started"
+		}
+]
+
+attendeeData = [
+		{
+			meeting_id: 1
+			attendee: [
+				{ user_id: 1 }
+				{ user_id: 2 }
+				{ user_id: 3 }
+			]
+		}
+]
+
+usersData = [
+		{
+			user_id: 1
+			name: "Afnizar"
+			email: "afnizar.hilmi@bukalapak.com"
+			avatar: "https://firebasestorage.googleapis.com/v0/b/bukaruang.appspot.com/o/500x500.jpg?alt=media&token=167fffaf-292c-48c3-b534-5759f2073868"
+			status: "user"
+		}
+]
+
 	
 bukaruang_db.put("/ruang", ruangData)
 bukaruang_db.put("/amenities", amenitiesData)
+bukaruang_db.put("/meeting", meetingData)
+bukaruang_db.put("/attendee", attendeeData)
+bukaruang_db.put("/users", usersData)
 
-# 	
-# bukaruang_db.get "/ruang", (value) ->
-# 	print value.available
+timeNow = new TextLayer
+	x: 1064
+	y: 115
+	fontSize: 48
+	fontFamily: "Noto Sans"
+	fontWeight: 700
+	letterSpacing: 0.0
+	parent: sketch.$01_Room_Display_In_Use
+	textAlign: "right"
+	color: "rgba(238,238,238,1)"
+	
+todayDate = new TextLayer
+	x: 895
+	y: 185
+	fontSize: 24
+	fontFamily: "Noto Sans"
+	parent: sketch.$01_Room_Display_In_Use
+	letterSpacing: 0.0
+	textAlign: "right"
+	color: "rgba(238,238,238,1)"
+	opacity: 0.800000011920929
+	
+
+# Time Right Now
+setTime = () ->
+	date = new Date
+	minute = date.getMinutes()
+	hour = date.getHours()
+	
+	if hour < 10
+		hour = '0' + hour 
+	
+	if minute < 10
+		minute = '0' + minute 
+
+	timeNow.text = "#{hour}:#{minute}"
+
+	Utils.delay 30, ->
+		setTime()
+
+setTime()
+	
+# Date
+setDate = () ->
+	today = new Date
+	dd = today.getDate()
+	dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	dm = dayNames[today.getDay() - 1]
+	yy = today.getFullYear()
+	monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	mm = monthNames[today.getMonth()];
+	todayDate.text = "#{dm}, #{dd} #{mm} #{yy}"
+
+setDate()
+
+todayDate.Align = "right"
+
+bukaruang_db.get "/ruang/0", (value) ->
+	room_name = new TextLayer
+		x: 126
+		y: 365
+		text: value.name
+		fontSize: 36
+		parent: sketch.$01_Room_Display_In_Use
+		fontFamily: "Noto Sans"
+		fontWeight: 700
+		textAlign: "left"
+		color: "rgba(238,238,238,1)"
+		
+bukaruang_db.get "/meeting/0", (value) ->
+	meeting_name = new TextLayer
+		x: 123
+		y: 414
+		text: value.desc
+		parent: sketch.$01_Room_Display_In_Use
+		fontSize: 96
+		fontFamily: "Noto Sans"
+		fontWeight: 700
+		textAlign: "left"
+		color: "rgba(238,238,238,1)"
+	
---
+++ Room Display.framer/imported/BukaRuang (Draft 2)@1x/layers.json.js
@@ -0,0 +1,575 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__['BukaRuang (Draft 2)@1x/layers.json.js'] = [
+   {
+      "objectId" : "8",
+      "name" : "layer-01_Room_Display___In_Use_",
+      "visible" : true,
+      "maskFrame" : {
+         "x" : -260,
+         "y" : 3460,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "layerFrame" : {
+         "x" : -260,
+         "y" : 3460,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "children" : [
+         {
+            "objectId" : "7",
+            "name" : "Headline",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 315,
+               "width" : 874,
+               "height" : 266
+            },
+            "children" : [
+               {
+                  "objectId" : "6",
+                  "name" : "Blue_Room",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 109,
+                     "y" : 315,
+                     "width" : 194,
+                     "height" : 50
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/6.png",
+                     "frame" : {
+                        "x" : 109,
+                        "y" : 315,
+                        "width" : 194,
+                        "height" : 50
+                     }
+                  }
+               }
+            ],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/7.png",
+               "frame" : {
+                  "x" : 109,
+                  "y" : 315,
+                  "width" : 874,
+                  "height" : 266
+               }
+            }
+         },
+         {
+            "objectId" : "5",
+            "name" : "Menu",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 1224,
+               "y" : 100,
+               "width" : 33,
+               "height" : 34
+            },
+            "children" : [],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/5.png",
+               "frame" : {
+                  "x" : 1224,
+                  "y" : 100,
+                  "width" : 33,
+                  "height" : 34
+               }
+            }
+         },
+         {
+            "objectId" : "4",
+            "name" : "Logo___On_Left",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 90,
+               "width" : 60,
+               "height" : 55
+            },
+            "children" : [
+               {
+                  "objectId" : "3",
+                  "name" : "Logo",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 109,
+                     "y" : 90,
+                     "width" : 60,
+                     "height" : 55
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/3.png",
+                     "frame" : {
+                        "x" : 109,
+                        "y" : 90,
+                        "width" : 60,
+                        "height" : 55
+                     }
+                  }
+               }
+            ],
+            "kind" : "group"
+         },
+         {
+            "objectId" : "2",
+            "name" : "Attendee",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 686,
+               "width" : 390,
+               "height" : 110
+            },
+            "children" : [],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/2.png",
+               "frame" : {
+                  "x" : 109,
+                  "y" : 686,
+                  "width" : 390,
+                  "height" : 110
+               }
+            }
+         },
+         {
+            "objectId" : "1",
+            "name" : "Time",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 856,
+               "y" : 96,
+               "width" : 330,
+               "height" : 108
+            },
+            "children" : [],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/1.png",
+               "frame" : {
+                  "x" : 856,
+                  "y" : 96,
+                  "width" : 330,
+                  "height" : 108
+               }
+            }
+         },
+         {
+            "objectId" : "0",
+            "name" : "Status",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 922,
+               "width" : 1148,
+               "height" : 102
+            },
+            "children" : [],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/0.png",
+               "frame" : {
+                  "x" : 109,
+                  "y" : 922,
+                  "width" : 1148,
+                  "height" : 102
+               }
+            }
+         }
+      ],
+      "kind" : "artboard",
+      "backgroundColor" : "rgba(0.75000, 0.75000, 0.75000, 1.00000)",
+      "imageType" : "png",
+      "image" : {
+         "path" : "images/8.png",
+         "frame" : {
+            "x" : -260,
+            "y" : 3460,
+            "width" : 1366,
+            "height" : 1024
+         }
+      }
+   },
+   {
+      "objectId" : "21",
+      "name" : "layer-01_Room_Display___In_Use___Iteration_2",
+      "visible" : true,
+      "maskFrame" : {
+         "x" : 3538,
+         "y" : 3484,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "layerFrame" : {
+         "x" : 3538,
+         "y" : 3484,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "children" : [
+         {
+            "objectId" : "20",
+            "name" : "Top",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 80,
+               "width" : 1148,
+               "height" : 55
+            },
+            "children" : [
+               {
+                  "objectId" : "19",
+                  "name" : "Menu_2",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 1224,
+                     "y" : 90,
+                     "width" : 33,
+                     "height" : 34
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/19.png",
+                     "frame" : {
+                        "x" : 1224,
+                        "y" : 90,
+                        "width" : 33,
+                        "height" : 34
+                     }
+                  }
+               },
+               {
+                  "objectId" : "18",
+                  "name" : "Time_2",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 1067,
+                     "y" : 90,
+                     "width" : 112,
+                     "height" : 36
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/18.png",
+                     "frame" : {
+                        "x" : 1067,
+                        "y" : 90,
+                        "width" : 112,
+                        "height" : 36
+                     }
+                  }
+               },
+               {
+                  "objectId" : "17",
+                  "name" : "Logo_3",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 109,
+                     "y" : 80,
+                     "width" : 60,
+                     "height" : 55
+                  },
+                  "children" : [
+                     {
+                        "objectId" : "16",
+                        "name" : "Logo_2",
+                        "visible" : true,
+                        "maskFrame" : null,
+                        "layerFrame" : {
+                           "x" : 109,
+                           "y" : 80,
+                           "width" : 60,
+                           "height" : 55
+                        },
+                        "children" : [],
+                        "kind" : "group",
+                        "imageType" : "png",
+                        "image" : {
+                           "path" : "images/16.png",
+                           "frame" : {
+                              "x" : 109,
+                              "y" : 80,
+                              "width" : 60,
+                              "height" : 55
+                           }
+                        }
+                     }
+                  ],
+                  "kind" : "group"
+               }
+            ],
+            "kind" : "group"
+         },
+         {
+            "objectId" : "15",
+            "name" : "Headline_2",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 111,
+               "y" : 257,
+               "width" : 463,
+               "height" : 371
+            },
+            "children" : [
+               {
+                  "objectId" : "14",
+                  "name" : "Status_2",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 111,
+                     "y" : 257,
+                     "width" : 151,
+                     "height" : 49
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/14.png",
+                     "frame" : {
+                        "x" : 111,
+                        "y" : 257,
+                        "width" : 151,
+                        "height" : 49
+                     }
+                  }
+               }
+            ],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/15.png",
+               "frame" : {
+                  "x" : 111,
+                  "y" : 257,
+                  "width" : 463,
+                  "height" : 371
+               }
+            }
+         },
+         {
+            "objectId" : "13",
+            "name" : "Attendee_2",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 109,
+               "y" : 740,
+               "width" : 381,
+               "height" : 100
+            },
+            "children" : [],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/13.png",
+               "frame" : {
+                  "x" : 109,
+                  "y" : 740,
+                  "width" : 381,
+                  "height" : 100
+               }
+            }
+         },
+         {
+            "objectId" : "12",
+            "name" : "Footer",
+            "visible" : true,
+            "maskFrame" : null,
+            "layerFrame" : {
+               "x" : 0,
+               "y" : 924,
+               "width" : 1366,
+               "height" : 100
+            },
+            "children" : [
+               {
+                  "objectId" : "11",
+                  "name" : "Room",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 1037,
+                     "y" : 954,
+                     "width" : 219,
+                     "height" : 40
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/11.png",
+                     "frame" : {
+                        "x" : 1037,
+                        "y" : 954,
+                        "width" : 219,
+                        "height" : 40
+                     }
+                  }
+               },
+               {
+                  "objectId" : "10",
+                  "name" : "information",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 109,
+                     "y" : 954,
+                     "width" : 248,
+                     "height" : 40
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/10.png",
+                     "frame" : {
+                        "x" : 109,
+                        "y" : 954,
+                        "width" : 248,
+                        "height" : 40
+                     }
+                  }
+               },
+               {
+                  "objectId" : "9",
+                  "name" : "Find",
+                  "visible" : true,
+                  "maskFrame" : null,
+                  "layerFrame" : {
+                     "x" : 403,
+                     "y" : 954,
+                     "width" : 261,
+                     "height" : 40
+                  },
+                  "children" : [],
+                  "kind" : "group",
+                  "imageType" : "png",
+                  "image" : {
+                     "path" : "images/9.png",
+                     "frame" : {
+                        "x" : 403,
+                        "y" : 954,
+                        "width" : 261,
+                        "height" : 40
+                     }
+                  }
+               }
+            ],
+            "kind" : "group",
+            "imageType" : "png",
+            "image" : {
+               "path" : "images/12.png",
+               "frame" : {
+                  "x" : 0,
+                  "y" : 924,
+                  "width" : 1366,
+                  "height" : 100
+               }
+            }
+         }
+      ],
+      "kind" : "artboard",
+      "backgroundColor" : "rgba(0.75000, 0.75000, 0.75000, 1.00000)"
+   },
+   {
+      "objectId" : "22",
+      "name" : "layer-01_Admin___Login",
+      "visible" : true,
+      "maskFrame" : {
+         "x" : -260,
+         "y" : 5462,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "layerFrame" : {
+         "x" : -260,
+         "y" : 5462,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "children" : [],
+      "kind" : "artboard",
+      "backgroundColor" : "rgba(1.00000, 1.00000, 1.00000, 1.00000)"
+   },
+   {
+      "objectId" : "23",
+      "name" : "layer-02_Admin___Dashboard",
+      "visible" : true,
+      "maskFrame" : {
+         "x" : 1146,
+         "y" : 5462,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "layerFrame" : {
+         "x" : 1146,
+         "y" : 5462,
+         "width" : 1366,
+         "height" : 1024
+      },
+      "children" : [],
+      "kind" : "artboard",
+      "backgroundColor" : "rgba(1.00000, 1.00000, 1.00000, 1.00000)"
+   },
+   {
+      "objectId" : "24",
+      "name" : "ERD",
+      "visible" : true,
+      "maskFrame" : null,
+      "layerFrame" : {
+         "x" : -2455,
+         "y" : 3471,
+         "width" : 1462,
+         "height" : 857
+      },
+      "children" : [],
+      "kind" : "group",
+      "imageType" : "png",
+      "image" : {
+         "path" : "images/24.png",
+         "frame" : {
+            "x" : -2455,
+            "y" : 3471,
+            "width" : 1462,
+            "height" : 857
+         }
+      }
+   }
+];
